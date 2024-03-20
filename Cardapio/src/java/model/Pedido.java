912cc97dddf434aeff82a493f38de0bd23457980
SAMUEL RODRIGUES DE ASSIS <346926@etb.net.br>
2024-03-20 11:20:31 -0300

    pedido

---
+++ Cardapio/src/java/model/Pedido.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author 346926
+ */
+public class Pedido {
+    
+    private Long id;
+    private String nome;
+    private Double valor;
+
+    public Pedido() {
+    }
+
+    public Pedido(Long id, String nome, Double valor) {
+        this.id = id;
+        this.nome = nome;
+        this.valor = valor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+    
+    
+    
+    
+}
